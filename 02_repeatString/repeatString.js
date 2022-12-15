021bd65e69c0cfdf21ad8709b7da149cf74439e6
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-15 00:59:39 +0000

    3 exercised done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+
+const string= 'hey'
+const num= 0
+
+
+function repeatString(string, num) {
+    if(num<0){
+        return('ERROR');
+    }
+    else{
+    let returnString='';
+
+    let i=1; 
+    while (i<=num) {
+        returnString += string;
+        i++;
+    }
+    return(returnString);
+    }
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
 
+let string=" ";
+
+const reverseString = function(string) {
+    let stringarray=string.split('');
+    let reversedarray=stringarray.reverse();
+    let reversed=reversedarray.join('');
+
+return(reversed)
 };
 
 // Do not edit below this line
