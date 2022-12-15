992608c07131db3f083be071a3f3d4dcb5375eba
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-15 23:55:08 +0000

    else if statements to check leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,28 @@
-const leapYears = function() {
+let Year=2022;
+const leapYears = function(Year) {
+
+    if(Year%4==0 && Year%400==0){ 
+        return(Boolean(Year%4==0 && Year%400==0));
+    }
+
+
+
+    else if(Year%4==0 && Year/400!=0){
+        return(Boolean(Year%4==0 && Year/400==0));    
+    }
+
+
+
+    else if(Year%4==0 && Year%100!=0){
+        return(Boolean(Year%4==0 && Year%100!=0));    
+    }
+
+
+    
+    else if(Year%4!=0){
+        return(Boolean(Year%4==0));
+    }
+
 
 };
 
