7830686c50e62c69399900b6a66729208cd9d09a
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-16 00:32:47 +0000

    wrote functions to convert temp and round them

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+let temperature = 0;
+
+const convertToCelsius = function(temperature) {
+  let celsius= (temperature-32)*5/9;
+  let roundedCelsius= Math.round(celsius*10)/10;
+  return(roundedCelsius);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit=(temperature*9/5 +32);
+  let roundedFahrenheit=Math.round(fahrenheit*10)/10;
+  return(roundedFahrenheit);
 };
 
 // Do not edit below this line
