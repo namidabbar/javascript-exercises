da42344d8603f7f02a4cd6860e90e2c4ee1f0f18
AnamK <119437655+namidabbar@users.noreply.github.com>
2022-12-16 00:13:24 +0000

    tidied up if statements to make it work

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,27 +1,21 @@
 let Year=2022;
 const leapYears = function(Year) {
 
-    if(Year%4==0 && Year%400==0){ 
-        return(Boolean(Year%4==0 && Year%400==0));
+    if(Year%4!=0){
+        return(Boolean(Year%4==0))
+    }
+    else if(Year%4==0 && Year%100!=0){
+        return(Boolean(Year%4==0))   
     }
 
-
+    else if(Year%4==0 && Year%400==0){ 
+        return(Boolean(Year%4==0 && Year%400==0));
+    }
 
     else if(Year%4==0 && Year/400!=0){
         return(Boolean(Year%4==0 && Year/400==0));    
     }
 
-
-
-    else if(Year%4==0 && Year%100!=0){
-        return(Boolean(Year%4==0 && Year%100!=0));    
-    }
-
-
-    
-    else if(Year%4!=0){
-        return(Boolean(Year%4==0));
-    }
 
 
 };
